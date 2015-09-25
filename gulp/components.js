3d5445c6ea340b74e9dc607627c05ec27678d5b6
Tushar Singh <tusharsingh@gmx.com>
2015-09-25 20:15:44 +0530

    update component tasks

--- gulp/components.js
+++ gulp/components.js
@@ -1,14 +1,17 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber')
-    sass = require('gulp-sass'),
-    notify = require('gulp-notify'),
-    minify = require('gulp-minify-css'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync');
+var gulp          = require('gulp'),
+    plumber       = require('gulp-plumber')
+    sass          = require('gulp-sass'),
+    react         = require('gulp-react'),
+    notify        = require('gulp-notify'),
+    minify        = require('gulp-minify-css'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    browserSync   = require('browser-sync');
 
 //Styles Browser Support
 var browserSupport = ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'];
+
+//Notification on build error
 var plumberConfig = {errorHandler: function(err){
   notify.onError({
     title:    "SASS",
@@ -19,26 +22,56 @@ var plumberConfig = {errorHandler: function(err){
   this.emit('end');
 }};
 
-// Component Task
+
 // Compiles component scss
-gulp.task('processComponents', function(){
-	gulp.src('src/components/examples/scss/main.scss')
-	.pipe(sourcemaps.init())
-	.pipe(plumber(plumberConfig))
-	.pipe(sass({ style: 'expanded', precision: 8 }))
+// =======================
+gulp.task('components.SASS', function(){
+	gulp.src('src/components/**/*.scss')
+	
+  //Prevent errors from breaking watch process
+  .pipe(plumber(plumberConfig))
+  
+  //compile SASS
+	.pipe(sass({ style: 'expanded', includePaths: ['src/sass'] }))
+  
+  //Autoprefixer
 	.pipe(autoprefixer(browserSupport))
-  .pipe(gulp.dest('src/components/examples/css'))
-	.pipe(rename({ suffix: '.min' }))
-	.pipe(minify())
-  .pipe(gulp.dest('src/components/examples/css'))
+  
+  .pipe(gulp.dest('build/components'))
+  
+  //BrowserSync injection
   .pipe(browserSync.stream());
 });
 
-// Watch Task for components
-gulp.task('watchComponents', function(){
-  browserSync.init();
-	gulp.watch('src/**/*.scss', ['processComponents']);
-	gulp.watch(['src/components/**/*.html','src/components/**/*.js']).on('change', browserSync.reload);
+
+// Copy HTML
+// =====================
+gulp.task('components.HTML', function(){
+	gulp.src('src/components/**/*.html')
+  .pipe(gulp.dest('build/components'));
 });
 
-gulp.task('components', ['processComponents', 'watchComponents'])
+
+// Compile Component JSX
+// =====================
+gulp.task('components.JSX', function(){
+	gulp.src('src/components/**/*.jsx')
+  .pipe(react())
+  .pipe(gulp.dest('build/components'));
+});
+
+
+// Watchman
+// =========================
+gulp.task('components.watch', function(){
+  browserSync.init();
+	gulp.watch('src/**/*.scss', ['components.SASS']);
+  
+	gulp.watch(['src/components/**/*.html'], ['components.HTML']);
+  
+  //Watch build directory
+  //so browsersync doesn't reload before gulp operations
+	gulp.watch(['build/components/**/*.js','build/components/**/*.html']).on('change', browserSync.reload);
+});
+
+gulp.task('components', ['components.SASS', 'components.JSX', 'components.HTML', 'components.watch'])
