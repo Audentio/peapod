31756719d96688bdc40eafe25af8dd5289564795
Tushar Singh <tusharsingh@gmx.com>
2015-10-09 04:52:38 +0530

    Added helper object

--- src/components/core.jsx
+++ src/components/core.jsx
@@ -3,15 +3,56 @@
  *  LICENSE: <%= package.licence %>
  */
 
+import ReactDOM from 'react-dom';
+
 var Pea_core = (function(){
  
 	window.peapod = {
 		basePath: '/',
 		suffix: '',
 		style: {},
-		options: {}
+		options: {},
+		helper: {}
 	};
 
 })();
+
+//control page scrolling
+//--
+//disables touch scrolling on touch enabled devices
+//uses css hax to disable scrolling without hiding scrollbar on no-touch devices
+peapod.helper.scrolling = function(allowScroll){
+
+	var  html = document.documentElement;
+
+	//Touch device
+	if (('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)) {
+		var disableScroll = function(e){
+			e.preventDefault()
+		};
+		//TBD
+	}
+	//no-touch device
+	else {
+		if(!allowScroll){
+			peapod.pageScrollPos = [window.pageXOffset, window.pageYOffset];
+		}
+
+		html.style.position 	= (allowScroll) ? '' : 'fixed';
+		html.style.top 			= (allowScroll) ? '' : -(peapod.pageScrollPos[1]) + 'px';
+		html.style.width 		= (allowScroll) ? '' : '100%';
+		html.style.overflowY 	= (allowScroll) ? '' : 'scroll';
+
+		if(allowScroll && peapod.pageScrollPos){
+			window.scroll(peapod.pageScrollPos[0],peapod.pageScrollPos[1])
+		}
+	}
+
+};
+
+//Key code map
+peapod.helper.keymap = {
+	'esc': 27
+}
 
 module.exports = Pea_core;
