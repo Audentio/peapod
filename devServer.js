249b261750bb3fa3ff9de3ac96282549e69f4970
Tushar Singh <rzr@rzr-mbp.local>
2016-01-04 23:36:51 +0530

    Changed port to avoid conflict with pinto

--- devServer.js
+++ devServer.js
@@ -7,10 +7,10 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(4000, 'localhost', function (err, result) {
+}).listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:4000');
+  console.log('Listening at localhost:3000');
 });
