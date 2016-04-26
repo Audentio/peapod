206efe1002d269e41c0ae7d5745945547297287a
Damion Yeatman <Damiony@audentio.com>
2016-04-26 20:36:31 +0100

    Updated headings to be cleaner.

--- src/peapod/components/heading.jsx
+++ src/peapod/components/heading.jsx
@@ -26,22 +26,7 @@ class Heading extends React.Component {
 
         var tagname = this.props.kind;
 
-        switch(tagname){
-            case "h1":
-                return (<h1 style={style.h1}>{this.props.text}</h1>);
-            case "h2":
-                return (<h2 style={style.h2}>{this.props.text}</h2>);
-            case "h3":
-                return (<h3 style={style.h3}>{this.props.text}</h3>);
-            case "h4":
-                return (<h4 style={style.h4}>{this.props.text}</h4>);
-            case "h5":
-                return (<h5 style={style.h5}>{this.props.text}</h5>);
-            case "h6":
-                return (<h6 style={style.h6}>{this.props.text}</h6>);
-            default:
-                return (<h1 style={style.main.h1}>{this.props.text}</h1>);
-        }
+        return React.createElement(tagname, {style: style[tagname]}, this.props.children)
 
     }
 
