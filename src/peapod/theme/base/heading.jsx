3ad1faaf5b10345df4617fb6bd6139931cbddeb2
Damion Yeatman <Damiony@audentio.com>
2016-04-26 20:32:17 +0100

    Added in headings

---
+++ src/peapod/theme/base/heading.jsx
@@ -0,0 +1,59 @@
+import {Sheet} from '../../stylesheet.jsx';
+
+var sheet = new Sheet,
+    main = sheet.addMain(),
+    h1 = sheet.addPart('h1'),
+    h2 = sheet.addPart('h2'),
+    h3 = sheet.addPart('h3'),
+    h4 = sheet.addPart('h4'),
+    h5 = sheet.addPart('h5'),
+    h6 = sheet.addPart('h6');
+
+//Conditions
+
+//Variables
+sheet.setValues({});
+
+
+main.addSelector({
+    common:{
+       marginBottom: '$gutter.internal'
+    }
+});
+h1.addSelector({
+    common:{
+       fontSize: '$font.size.xxxlarge'
+    }
+});
+
+h2.addSelector({
+    common:{
+       fontSize: '$font.size.xxlarge'
+    }
+});
+
+h3.addSelector({
+    common:{
+       fontSize: '$font.size.xlarge'
+    }
+});
+
+h4.addSelector({
+    common:{
+       fontSize: '$font.size.large'
+    }
+});
+
+h5.addSelector({
+    common:{
+       fontSize: '$font.size.normal'
+    }
+});
+
+h6.addSelector({
+    common:{
+       fontSize: '$font.size.small'
+    }
+});
+
+module.exports = sheet;
