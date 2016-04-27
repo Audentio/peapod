7907c3dc95f1bddcb90937f308571574a073a6fc
Damion Yeatman <Damiony@audentio.com>
2016-04-27 18:37:41 +0100

    Updated headings margins

--- src/peapod/theme/base/heading.jsx
+++ src/peapod/theme/base/heading.jsx
@@ -1,7 +1,6 @@
 import {Sheet} from '../../stylesheet.jsx';
 
 var sheet = new Sheet,
-    main = sheet.addMain(),
     h1 = sheet.addPart('h1'),
     h2 = sheet.addPart('h2'),
     h3 = sheet.addPart('h3'),
@@ -12,47 +11,53 @@ var sheet = new Sheet,
 //Conditions
 
 //Variables
-sheet.setValues({});
-
-
-main.addSelector({
-    common:{
-       marginBottom: '$gutter.internal'
+sheet.setValues({
+    common: {
+        heading: {
+            textTransform: 'uppercase'
+        }
     }
 });
+
 h1.addSelector({
     common:{
-       fontSize: '$font.size.xxxlarge'
+       fontSize: '$font.size.xxxlarge',
+       marginBottom:'$font.size.xxxlarge',
     }
 });
 
 h2.addSelector({
     common:{
-       fontSize: '$font.size.xxlarge'
+       fontSize: '$font.size.xxlarge',
+       marginBottom:'$font.size.xxlarge',
     }
 });
 
 h3.addSelector({
     common:{
-       fontSize: '$font.size.xlarge'
+       fontSize: '$font.size.xlarge',
+       marginBottom:'$font.size.xlarge',
     }
 });
 
 h4.addSelector({
     common:{
-       fontSize: '$font.size.large'
+       fontSize: '$font.size.large',
+       marginBottom:'$font.size.large',
     }
 });
 
 h5.addSelector({
     common:{
-       fontSize: '$font.size.normal'
+       fontSize: '$font.size.normal',
+       marginBottom:'$font.size.normal',
     }
 });
 
 h6.addSelector({
     common:{
-       fontSize: '$font.size.small'
+       fontSize: '$font.size.small',
+       marginBottom:'$font.size.small',
     }
 });
 
