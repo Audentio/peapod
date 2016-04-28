dd77598c7b6200af2fdbef881bfe842a7837f938
Damion Yeatman <Damiony@audentio.com>
2016-04-28 16:46:33 +0100

    Added in menu on hover as well as the onclick though the portal component.

--- src/peapod/components/menu.jsx
+++ src/peapod/components/menu.jsx
@@ -4,15 +4,52 @@ var Pod_Styler = require('../styler.jsx');
 var Wrapper = require('../wrapper.jsx')
 
 class Menu extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            show: false
+        }
+    }
+
+    mouseEnter() {
+        this.setState({show: true})
+    }
+    mouseLeave() {
+        this.setState({show: false})
+    }
 
     render() {
         var style = Pod_Styler.getStyle(this);
-
-        return (
+        var children =  (this.state.show) ? (
             <div style={style.main}>
                 {this.props.children}
             </div>
-        )
+        ) : "";
+
+        if (this.props.portal) {
+            return(
+                <Pod.portal
+                    trigger={this.props.trigger}
+                    closeOnOutsideClick={true}
+                    noArrow={true}
+                >
+                    <div style={style.portal}>
+                        {this.props.children}
+                    </div>
+                </Pod.portal>
+            )
+        } else {
+            return (
+                <div style={style.trigger}
+                    onMouseEnter={this.mouseEnter.bind(this)}
+                    onMouseLeave={this.mouseLeave.bind(this)}
+                >
+                    {this.props.trigger}
+                    {children}
+                </div>
+            )
+        }
     }
 };
 
--- src/peapod/theme/base/menu.js
+++ src/peapod/theme/base/menu.js
@@ -1,11 +1,14 @@
 import {Sheet} from '../../stylesheet.jsx';
 
 var sheet = new Sheet,
-    main = sheet.addMain();
+    main = sheet.addMain(),
+    portal = sheet.addPart('portal'),
+    trigger = sheet.addPart('trigger');
 
 //Variables
 sheet.setValues({
-    common: {}
+    common: {
+    }
 });
 
 main.addSelector({
@@ -15,8 +18,25 @@ main.addSelector({
         padding: '16px 0',
         borderRadius: '$border.radius.small',
         zIndex: 3,
+        position: 'absolute'
+    }
+});
+
+portal.addSelector({
+    common: {
+        background: '#fff',
+        boxShadow: '0 1px 3px rgba(0,0,0,0.16), 0 1px 3px rgba(0,0,0,0.23)',
+        padding: '16px 0',
+        borderRadius: '$border.radius.small',
+        zIndex: 3,
         position: 'relative'
     }
 });
+
+trigger.addSelector({
+    common: {
+        display: 'inline-block'
+    }
+})
 
 module.exports = sheet;
