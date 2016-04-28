dbface2f596bd48fe7a35571d4c30a5bf3a64c90
Damion Yeatman <Damiony@audentio.com>
2016-04-28 16:35:04 +0100

    Added submenu on hover

--- src/peapod/components/subMenu.jsx
+++ src/peapod/components/subMenu.jsx
@@ -4,13 +4,35 @@ var Pod_Styler = require('../styler.jsx');
 var Wrapper = require('../wrapper.jsx')
 
 class SubMenu extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            show: false
+        }
+    }
+
+    mouseEnter() {
+        this.setState({show: true})
+    }
+    mouseLeave() {
+        this.setState({show: false})
+    }
 
     render() {
         var style = Pod_Styler.getStyle(this);
+        var children = (this.state.show) ? this.props.children : '';
+
 
         return (
-            <div style={style.main}>
-                {this.props.children}
+            <div
+                onMouseEnter={this.mouseEnter.bind(this)}
+                onMouseLeave={this.mouseLeave.bind(this)}
+            >
+                {this.props.trigger}
+                <div style={style.main}>
+                    {children}
+                </div>
             </div>
         )
     }
