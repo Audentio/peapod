b699f24b5109cc628a975584c404ec77bbf762d5
Damion Yeatman <damion@yeatman.me>
2016-05-02 23:00:45 +0100

    Added in simpler cards

--- examples/sections.jsx
+++ examples/sections.jsx
@@ -313,6 +313,56 @@ sections.push(
 )
 
 sections.push(
+    <div className="section grey" key={"simplecards"}>
+
+        <Pod.heading>Simple Cards</Pod.heading>
+
+        <Pod.card styler={{padded:true, style:{width: '350px'}}} title="Simple Card" actionBar={(
+            <div>
+                <Pod.button styler={{type: 'icon'}} label={
+                        (<Pod.icon styler={{ style: { lineHeight: 'inherit' } }}>bookmark</Pod.icon>)
+                    } />
+
+                <Pod.button styler={{type: 'icon'}} label={
+                    (<Pod.icon styler={{ style: { lineHeight: 'inherit' } }}>get_app</Pod.icon>)
+                } />
+            </div>
+        )}>
+            <Pod.photo src="image.png"/>
+        </Pod.card>
+
+    <Pod.card styler={{style:{width: '350px'}}} actionBar={(
+            <div>
+                <Pod.button styler={{type: 'icon'}} label={
+                        (<Pod.icon styler={{ style: { lineHeight: 'inherit' } }}>favorite</Pod.icon>)
+                    } />
+
+                <Pod.button styler={{type: 'icon'}} label={
+                    (<Pod.icon styler={{ style: { lineHeight: 'inherit' } }}>get_app</Pod.icon>)
+                } />
+            </div>
+        )}>
+            <Pod.photo src="image.png"/>
+        </Pod.card>
+
+    <Pod.card styler={{padded:true, style:{width: '350px'}}} title="Simple Card" actionBar={(
+            <div>
+                <Pod.button label="Button" />
+
+                <Pod.button label="Another Button" />
+            </div>
+        )}>
+        <Pod.paragraph>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Pod.paragraph>
+    </Pod.card>
+
+    <Pod.card styler={{padded:true, style:{width: '350px'}}} title="Simple Card">
+        <Pod.paragraph>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Pod.paragraph>
+    </Pod.card>
+
+    </div>
+)
+
+sections.push(
 	<div className="section grey" key={"lists"}>
 
 		<Pod.heading>Lists</Pod.heading>
--- src/peapod/theme/base/card.js
+++ src/peapod/theme/base/card.js
@@ -1,10 +1,14 @@
 import {Sheet} from '../../stylesheet.js';
 
 var sheet = new Sheet,
-    main = sheet.addMain();
+    main = sheet.addMain(),
+    actionBar = sheet.addPart('actionBar'),
+    title = sheet.addPart('title'),
+    content = sheet.addPart('content');
 
 //Conditions
 sheet.addCondition('disguised').addStyler({disguised: true});
+sheet.addCondition('padded').addStyler({padded: true});
 
 //Variables
 sheet.setValues({});
@@ -27,5 +31,33 @@ main.addSelector({
         boxShadow: 'none'
     }
 })
+// .addSelector({
+//     condition: ['padded'],
+//     common: {
+//         padding: '16px'
+//     }
+// })
+
+title.addSelector({
+    common: {
+        paddingTop: '16px',
+        paddingLeft: '16px',
+        paddingRight: '16px'
+    }
+});
+
+content.addSelector({
+    condition: ['padded'],
+    common: {
+        paddingLeft: '16px',
+        paddingRight: '16px'
+    }
+});
+
+actionBar.addSelector({
+    common: {
+        padding: 8
+    }
+});
 
 module.exports = sheet;
