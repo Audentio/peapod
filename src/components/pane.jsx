78a9ff89e1741bb43098afa61774ddaf2fb857e5
Kyler Carlson <kylerc@audentio.com>
2016-05-05 10:33:59 -0500

    added in panes

---
+++ src/components/pane.jsx
@@ -0,0 +1,68 @@
+/* Copyright <%= package.year %>, Audentio, LLC.
+* All rights reserved.
+*
+* LICENSE: <%= package.licence %>
+*/
+
+
+
+//Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var Pod_Styler = require('../styler.js');
+var Wrapper = require('../wrapper.jsx')
+
+
+var Pane = React.createClass({
+
+	getInitialState: function() {
+		return {
+			width: -1
+		}
+	},
+
+	contextTypes: {
+		width: React.PropTypes.number
+	},
+
+	getChildContext() {
+		return {width: this.state.width};
+	}
+
+	componentDidMount() {
+
+		//initial check
+		this.widthCheck();
+
+		// bind this to width check always
+		this.widthCheck = this.widthCheck.bind(this)
+
+		//start listening for viewport events
+		window.addEventListener('resize', this.widthCheck)
+
+	}
+
+	widthCheck() {
+		var element = this.refs.pane
+		var width = element.offsetWidth;
+
+		if (width != this.state.width) ){
+			this.setState({width: width});
+		}
+	}
+
+	rrender: function() {
+		var {styler, children, ...other} = this.props,
+		style = Pod_Styler.getStyle(this);
+
+		return (
+			<div {...other} style={style.main} ref="pane">
+				{this.props.children}
+			</div>
+		);
+	}
+
+});
+
+module.exports = Wrapper(Pane);
