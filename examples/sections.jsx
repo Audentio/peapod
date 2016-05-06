ef888e62da94fdfc43087d288e1ce8f3e90f725e
Damion Yeatman <damion@yeatman.me>
2016-05-07 00:55:50 +0100

    Updated Examples
    Set the example page to load all of the contents of the
    `sectionComponents` folder.

--- examples/sections.jsx
+++ examples/sections.jsx
@@ -4,69 +4,33 @@ import Paragraph from 'components/paragraph'
 import Strong from 'components/strong'
 import Code from 'components/code'
 
+var components = []
+var req = require.context('./sectionComponents', false, /^\.\/.*\.jsx$/),
+    fileNames = req.keys();
 
-var comps = ['hero']
+for (var i = 0, len = fileNames.length; i < len; i++) {
+    var fileName = fileNames[i].replace('./', '').replace('.jsx', ''),
+        componentName = fileName.charAt(0).toUpperCase() + fileName.slice(1);
+        componentName = componentName+'Section'
 
-// import sections
-import HeroSection from './sectionComponents/hero.jsx'
-import MediaSection from './sectionComponents/media.jsx'
-import TypographySection from './sectionComponents/typography.jsx'
-import FromsSection from './sectionComponents/forms.jsx'
-import EmbedSection from './sectionComponents/embed.jsx'
-import GridSection from './sectionComponents/grid.jsx'
-import DeviceSection from './sectionComponents/device.jsx'
-import BreadcrumbSection from './sectionComponents/breadcrumb.jsx'
-import BlockquoteSection from './sectionComponents/blockquote.jsx'
-import ChipsSection from './sectionComponents/chips.jsx'
-import FixedSection from './sectionComponents/fixed.jsx'
-import MenusSection from './sectionComponents/menus.jsx'
-import ModalsSection from './sectionComponents/modals.jsx'
-import CardsSection from './sectionComponents/cards.jsx'
-import ListsSection from './sectionComponents/lists.jsx'
-import ButtonsSection from './sectionComponents/buttons.jsx'
-import PhotosSection from './sectionComponents/photos.jsx'
-import IconsSection from './sectionComponents/icons.jsx'
-import MicroSection from './sectionComponents/micro.jsx'
-import LabelSection from './sectionComponents/label.jsx'
-import TabsSection from './sectionComponents/tabs.jsx'
-import AlertsSection from './sectionComponents/alerts.jsx'
-import ScrollableSection from './sectionComponents/scrollable.jsx'
-import ProgressSection from './sectionComponents/progress.jsx'
+        window[componentName] = req('./' + fileName + '.jsx').default;
+        components.push(React.createElement(window[componentName], null));
+}
+
+
+// var lazyload = (
+// <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+// <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+// <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+// <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+// <Pod.photo src="mrRobot.jpg" lazy={true} caption="Lazy load!" hidpiData={false} />
+// )
 
 class Sections extends React.Component {
     render () {
         return (
             <div>
-                <HeroSection />
-                <MediaSection />
-                <TypographySection />
-                <FromsSection />
-                <ScrollableSection />
-                <EmbedSection />
-                <DeviceSection />
-                <BreadcrumbSection />
-                <BlockquoteSection />
-                <ChipsSection />
-                <FixedSection />
-                <PhotosSection />
-                <MenusSection />
-                <GridSection />
-                <ModalsSection />
-                <CardsSection />
-                <ListsSection />
-                <ButtonsSection />
-                <IconsSection />
-                <MicroSection />
-                <LabelSection />
-                <TabsSection />
-                <AlertsSection />
-                <ProgressSection />
-
-                <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        		<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        		<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        		<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-        		<Pod.photo src="mrRobot.jpg" lazy={true} caption="Lazy load!" hidpiData={false} />
+                {components}
             </div>
         )
     }
