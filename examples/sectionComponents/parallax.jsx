84e47436e345268b736707f8bfa91e8ba027596c
Damion Yeatman <damion@yeatman.me>
2016-05-11 01:24:30 +0100

    Parallax Updates
    The Parallax was slow and caused a lot of re-paints. This update fixes
    that.

--- examples/sectionComponents/parallax.jsx
+++ examples/sectionComponents/parallax.jsx
@@ -9,7 +9,7 @@ export default class ParallaxSection extends React.Component {
 
     render () {
         return (
-            <Pod.parallax background={(<div style={{width:'100%', height: '100%', background: 'url(mrRobot.jpg)', backgroundSize: 'cover', backgroundPosition:'center center'}}></div>)}>
+            <Pod.parallax background={(<div style={{width:'100%', height: '100%', backgroundImage: 'url(mrRobot.jpg)', backgroundSize: 'cover', backgroundPosition:'center center'}}></div>)}>
                 <Pod.center><Pod.heading style={{style: {color: 'white'}}}>Parallax</Pod.heading></Pod.center>
             </Pod.parallax>
         )
