38291f286ef33e733cd2d7d61263bc814822e59e
Damion Yeatman <damion@yeatman.me>
2016-05-12 19:42:44 +0100

    style.js for Parallax

---
+++ src/theme/peapod/parallax/style.js
@@ -0,0 +1,47 @@
+import {Sheet} from 'stylesheet.js';
+
+module.exports = function(sheetName) {
+	var sheet = new Sheet(sheetName),
+	    main = sheet.addMain(),
+	    front = sheet.addPart('front'),
+	    back = sheet.addPart('back');
+
+	//Conditions
+	//Variables
+	sheet.setValues({
+	    height: '100vh',
+	    width: '100vw'
+	});
+
+	main.addSelector({
+	    common: {
+	        height: '$parallax.height',
+	        width: '$parallax.width',
+	        overflow: 'hidden',
+	        position: 'relative',
+	        background:'red'
+	    }
+	});
+	front.addSelector({
+	    common: {
+	        height: 'calc( ' + Pod_Vars.get('parallax.height') + ' * 2)',
+	        width: '$parallax.width',
+	        position: 'absolute',
+	        top: 0, left: 0,
+	        transform: '',
+	        willChange: 'transform'
+	    }
+	});
+	back.addSelector({
+	    common: {
+	        height: 'calc( ' + Pod_Vars.get('parallax.height') + ' * 2)',
+	        width: '$parallax.width',
+	        position: 'absolute',
+	        top: 0, left: 0,
+	        transform: '',
+	        willChange: 'transform'
+	    }
+	});
+
+	return sheet;
+}
