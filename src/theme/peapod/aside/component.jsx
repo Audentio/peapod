5b927df083743648202eab2b9be260eb9b61a492
Damion Yeatman <damion@yeatman.me>
2016-05-16 23:41:52 +0100

    Added html5 elements
    Article, Aside, Footer, Header, Main, Nav, Mark

---
+++ src/theme/peapod/aside/component.jsx
@@ -0,0 +1,21 @@
+/*! Peapod v<%= package.version %>
+ *  Copyright Audentio <%= package.year %>
+ *  LICENSE: <%= package.licence %>
+ */
+
+import React from 'react';
+import Pod_Styler from 'styler.js';
+
+module.exports = class Aside extends React.Component {
+	render() {
+		var {styler, children, ...other} = this.props,
+			style = Pod_Styler.getStyle(this);
+
+		return (
+			<aside {...other} style={style.main}>
+				{this.props.children}
+			<aaside>
+		);
+	}
+
+};
