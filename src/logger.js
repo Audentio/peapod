8fe44443a6a12e3ecd0b871a5ac2fab586514da4
Tushar Singh <tusharsingh@gmx.com>
2016-05-20 06:33:20 +0530

    Logger fix

--- src/logger.js
+++ src/logger.js
@@ -3,21 +3,47 @@
 *  Copyright Audentio <%= package.year %>
 *  LICENSE: <%= package.licence %>
 */
-
-const showLogs = false;
-
+/*
 const Logger = {
-    log: console.log,
-    info: (console.error !== undefined) ? console.info : (message) => Logger.log(`INFO: ${message}`),
-    warn: (console.warn !== undefined) ? console.warn : (message) => Logger.log(`WARNING: ${message}`),
-    error: (console.error !== undefined) ? console.error : (message) => Logger.log(`ERROR: ${message}`),
-};
+    log: (...args) => console.log(...args),
+    info:
+        (console.info !== undefined) ?
+        (...args) => console.info(...args)
+        :
+        (message) => console.log(`INFO: ${message}`),
 
-if (showLogs === false) {
-    Logger.log = () => false;
-    Logger.info = () => false;
-    Logger.warn = () => false;
-    Logger.error = () => false;
+    warn:
+        (console.warn !== undefined) ?
+        (...args) => console.warn(...args)
+        :
+        (message) => console.log(`WARNING: ${message}`),
+    error:
+        (console.error !== undefined) ?
+        (...args) => console.error(...args)
+        :
+        (message) => console.log(`ERROR: ${message}`),
+    group:
+        (console.group !== undefined) ?
+        (...args) => console.group(...args)
+        :
+        () => false,
+    groupCollapsed:
+        (console.groupCollapsed !== undefined) ?
+        (...args) => console.groupCollapsed(...args)
+        :
+        () => false,
+    groupEnd:
+        (console.groupEnd !== undefined) ?
+        (...args) => console.groupEnd(...args)
+        :
+        () => false,
+};*/
+
+const showLogs = true;
+const Logger = console;
+
+if (showLogs) {
+    module.exports = Logger;
+} else {
+    module.exports = {};
 }
-
-export default Logger;
