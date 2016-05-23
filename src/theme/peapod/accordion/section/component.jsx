797e0c1881a1b0bb3d54294018aa493a6e0d6d6f
Damion Yeatman <damion@yeatman.me>
2016-05-23 20:02:55 +0100

    Accordion Updates & Materialise

--- src/theme/peapod/accordion/section/component.jsx
+++ src/theme/peapod/accordion/section/component.jsx
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import Pod_Styler from 'styler.js';
+import { Card, Icon } from 'components.js';
 
 module.exports = class Accordion_Section extends React.Component {
     static propTypes = {
@@ -16,24 +17,22 @@ module.exports = class Accordion_Section extends React.Component {
 
         this.state = {
             open: false,
-            animateClose: false,
         };
 
         this.titleClick = this.titleClick.bind(this);
     }
 
     componentWillReceiveProps(nextProps) {
-        if (!nextProps.active) {
-            this.setState({ open: false, animateClose: true });
-        }
+        this.setState({ open: nextProps.active });
     }
     componentWillUnmount() {
         // setTimeout();
+        // this.setState({ open:this.props.active });
     }
 
     titleClick() {
-        this.setState({ open: true });
         this.props.onTitleClick(this.props.id);
+        // this.setState({ open: !this.state.open });
     }
 
     render() {
@@ -42,15 +41,30 @@ module.exports = class Accordion_Section extends React.Component {
 
         // const contentStyle = Object.assign({}, style.content, style.contentLast);
 
-        const content = (this.props.active || this.state.animateClose) ? (<div style={style.content}>{this.props.children}</div>) : '';
+        // const content = (this.props.active || this.state.animateClose) ? () : '';
+
+        let subtitle;
+        const subtitleProp = (typeof this.props.subtitle === 'string') ? [this.props.subtitle] : this.props.subtitle;
+        if (subtitleProp) {
+            subtitle = [];
+            for (var i = 0; i < subtitleProp.length; i++) {
+                subtitle.push(<div style={style.subtitle}>{subtitleProp[i]}</div>);
+            }
+        }
 
         return (
-            <div style={style.main}>
-                <div style={style.title} onClick={this.titleClick}>{this.props.title}</div>
-                <div style={style.contentWrap}>
-                    {content}
+            <Card styler={{ style: style.main }}>
+                <div style={style.title} onClick={this.titleClick}>
+                    <Icon styler={{ style: style.icon }}>keyboard_arrow_down</Icon>
+                    <div style={Object.assign({}, style.subtitle, style.maintitle)}>
+                        {this.props.title}
+                    </div>
+                    {subtitle}
                 </div>
-            </div>
+                <div style={style.contentWrap}>
+                    <div style={style.content}>{this.props.children}</div>
+                </div>
+            </Card>
         );
     }
 
