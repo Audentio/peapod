8f50c30eb48f2d08433c4757aac6fed35b3e4820
Damion Yeatman <damion@yeatman.me>
2016-05-23 21:24:21 +0100

    Examples cleanup

--- src/theme/peapod/chip/example.jsx
+++ src/theme/peapod/chip/example.jsx
@@ -9,8 +9,8 @@ module.exports = class ChipExample extends React.Component {
                 <Pod.ContentWrap>
                     <Pod.Chip>This is a Chip</Pod.Chip>
                     <Pod.Chip deleteTrigger>Chip with Delete</Pod.Chip>
-                    <Pod.Chip photo="profile@2x.jpg">Chip with Photo</Pod.Chip>
-                    <Pod.Chip deleteTrigger photo="profile@2x.jpg">Chip with Delete & Photo</Pod.Chip>
+                    <Pod.Chip photo="assets/media/profile@2x.jpg">Chip with Photo</Pod.Chip>
+                    <Pod.Chip deleteTrigger photo="assets/media/profile@2x.jpg">Chip with Delete & Photo</Pod.Chip>
                 </Pod.ContentWrap>
             </div>
         );
--- src/theme/peapod/parallax/example.jsx
+++ src/theme/peapod/parallax/example.jsx
@@ -7,7 +7,7 @@ module.exports = class ParallaxExample extends React.Component {
         return (
             <Pod.Parallax
                 background={(
-                    <div style={{ width: '100%', height: '100%', backgroundImage: 'url(demo/bg2.jpg)', backgroundSize: 'cover', backgroundPosition: 'center center' }}></div>
+                    <div style={{ width: '100%', height: '100%', backgroundImage: 'url(assets/media/bg2.jpg)', backgroundSize: 'cover', backgroundPosition: 'center center' }}></div>
                 )}
             >
                 <Pod.Center>
