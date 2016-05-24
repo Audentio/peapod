4f4e6b4300c1130c10aa26716739c532828d8611
Damion Yeatman <damion@yeatman.me>
2016-05-24 19:42:27 +0100

    Button cursor update

--- src/theme/peapod/button/style.js
+++ src/theme/peapod/button/style.js
@@ -13,7 +13,7 @@ module.exports = function (sheetName) {
     sheet.addCondition('block').addStyler({ block: true });
     sheet.addCondition('round').addStyler({ round: true });
     sheet.addCondition('disabled').addStyler({ disabled: true });
-    sheet.addCondition('notDisabled').addStyler({ disabled: undefined });
+    sheet.addCondition('notDisabled').addStyler({ disabled: ['!=', true] });
     sheet.addCondition('dense').addStyler({ dense: true });
     sheet.addCondition('dialog').addStyler({ dialog: true });
 
