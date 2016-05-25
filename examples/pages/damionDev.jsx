f9a797539bb2bdd9efe35d2b4ffcd2ba926e76c2
Damion Yeatman <damion@yeatman.me>
2016-05-25 16:52:09 +0100

    Dev server updates

--- examples/pages/damionDev.jsx
+++ examples/pages/damionDev.jsx
@@ -55,37 +55,6 @@ export default class DamionDev extends React.Component {
 
                 <Pod.Section styler={{ mainStyle: { background: 'transparent' } }}>
                     <Pod.ContentWrap>
-                        <Pod.Card styler={{ style: { width: '700px', margin: '0 auto', display: 'block' } }}>
-                            <Pod.Stepper skippable>
-                                <Pod.Stepper_Step title="Apples" validation={() => true}>
-                                    {/* <Pod.Label>Email Address</Pod.Label>*/}
-                                    <Pod.Input
-                                        type="email"
-                                        validate
-                                        label="Hey"
-                                        placeholder="email"
-                                        required
-                                        onChange={this.validateInput}
-                                    />
-                                </Pod.Stepper_Step>
-
-                                <Pod.Stepper_Step title="Pears">
-                                    <Pod.Paragraph>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    </Pod.Paragraph>
-                                </Pod.Stepper_Step>
-
-                                <Pod.Stepper_Step title="Bananas">
-                                    <Pod.Paragraph>Card Number</Pod.Paragraph>
-                                    <Pod.Input label="Hey" placeholder="***************"></Pod.Input>
-                                </Pod.Stepper_Step>
-                            </Pod.Stepper>
-                        </Pod.Card>
                     </Pod.ContentWrap>
                 </Pod.Section>
 
