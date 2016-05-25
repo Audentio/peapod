7fb75a2a72c07aca2896f8eb39d7af0f3830df90
Damion Yeatman <damion@yeatman.me>
2016-05-25 15:30:00 +0100

    Portal, Datepicker
    Updated portal to fix the issues on #69
    Removed margin from date picker.

--- src/theme/peapod/datePicker/style.js
+++ src/theme/peapod/datePicker/style.js
@@ -3,6 +3,7 @@ import { Sheet } from 'utility/stylesheet.js';
 module.exports = function (sheetName) {
     const sheet = new Sheet(sheetName);
     const main = sheet.addMain();
+    const card = sheet.addPart('card');
     const calendar = sheet.addPart('calendar');
 
     // Conditions
@@ -13,6 +14,14 @@ module.exports = function (sheetName) {
     main.addSelector({
         common: {
             display: 'inline-block',
+        },
+    });
+    card.addSelector({
+        common: {
+            marginTop: '$gutter.internal',
+            marginRight: 0,
+            marginBottom: 0,
+            marginLeft: 0,
         },
     });
     calendar.addSelector({
