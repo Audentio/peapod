e8d51f10cff353140043005a5bd36dc5ec175b20
Damion Yeatman <damion@yeatman.me>
2016-05-27 23:48:55 +0100

    Lazy Overhall

--- examples/pages/damionDev.jsx
+++ examples/pages/damionDev.jsx
@@ -7,60 +7,97 @@ export default class DamionDev extends React.Component {
 
     constructor() {
         super();
-        this.state = { validOne: false };
+        this.state = { loaded: false };
 
-        this.onMouseEnter = this.onMouseEnter.bind(this);
-        this.onMouseLeave = this.onMouseLeave.bind(this);
+        // this.onMouseEnter = this.onMouseEnter.bind(this);
+        // this.onMouseLeave = this.onMouseLeave.bind(this);
     }
 
-    onMouseEnter() {
-        this.setState({ showtool: true });
-    }
-    onMouseLeave() {
-        this.setState({ showtool: false });
-    }
-    validateInput(e) {
-        this.setState({ validOne: e.isValid });
+    componentDidMount() {
+        const elem = this.refs.apples;
+
+        const iObserver = new IntersectionObserver((crap) => {
+
+            console.log(elem, crap);
+            this.setState({ loaded: crap[0].intersectionRatio > 0 });
+
+        }, { threshold: [0], rootMargin: '200% 0%' });
+        iObserver.observe(elem);
     }
 
     render() {
+
+        const currentContent = (this.state.loaded) ? <div>
+            <Pod.Card styler={{ style: { width: '700px', display: 'block' } }}>
+                <Pod.Stepper skippable singleForm >
+                    <Pod.Stepper_Step title="Apples" validation={() => true}>
+                        <Pod.Paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </Pod.Paragraph>
+                        <Pod.Paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </Pod.Paragraph>
+                        <Pod.Paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </Pod.Paragraph>
+                        <Pod.Paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </Pod.Paragraph>
+                    </Pod.Stepper_Step>
+
+                    <Pod.Stepper_Step title="Pears" validation={() => false}>
+                        <Pod.Paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </Pod.Paragraph>
+                    </Pod.Stepper_Step>
+
+                    <Pod.Stepper_Step title="Bananas" subtitle="Bananannannanana" validation={() => true}>
+                        <Pod.Paragraph>Card Number</Pod.Paragraph>
+                        <Pod.Input label="Hey" placeholder="***************"></Pod.Input>
+                    </Pod.Stepper_Step>
+                </Pod.Stepper>
+            </Pod.Card>
+        </div> : <div>Loading</div>;
+
         return (
             <div key={'dev'} style={{ marginTop: '50px' }}>
-                {/* <Pod.Json
-                    json={{
-                    DatePicker: {
-                    date: '2017-11-20',
-                    },
-                    Menu: {
-                    JsonParse: ['trigger'],
-                    trigger: {
-                    Button: {
-                    label: 'on hover',
-                    },
-                    },
-                    json: [
-                    { text: 'Hello World', href: '#' },
-                    { text: 'Hello World 36', href: '#',
-                    children: [
-                    { text: 'Hello World 387', href: '#' },
-                    { text: 'Hello World 123', href: '#', subtext: '2' },
-                    ],
-                    },
-                    { text: 'Hello World 387', href: '#' },
-                    { text: 'Hello World 123', href: '#', subtext: '2' },
-                    ],
-                    },
-                    }}
-                />*/}
 
                 <Pod.Section styler={{ mainStyle: { background: 'transparent' } }}>
                     <Pod.ContentWrap>
+                        <div style={{
+                            margin: '100vh 0 0',
+                        }} ref="apples"
+                        >
+                            <Pod.Center>{currentContent}</Pod.Center>
+                        </div>
+
                     </Pod.ContentWrap>
                 </Pod.Section>
-
-                {/* <Pod.List style="square">
-                    <Pod.List_Item>Hey</Pod.List_Item>
-                </Pod.List>*/}
 
             </div>
         );
--- src/theme/peapod/lazy/component.jsx
+++ src/theme/peapod/lazy/component.jsx
@@ -16,65 +16,82 @@ module.exports = componentName => class Pod_Component extends React.Component {
 
     static displayName = componentName;
 
-    static defaultProps = {
-        visible: false,
-        distance: 100,
-        placeholder: false,
-    }
-
     constructor() {
         super();
 
         this.state = {
-            visible: false,
+            visible: this.props.visible,
         };
     }
 
+    static defaultProps = {
+        visible: false,
+        // distance: 100,
+        placeholder: false,
+    }
+
+    static propTypes = {
+        placeholder: React.PropTypes.any,
+        visible: React.PropTypes.bool,
+        children: React.PropTypes.any,
+    }
+
     componentDidMount() {
+        const elem = this.refs.LazyElement;
 
-        // initial check
-        this.lazyCheck();
-
-        // bind this to lazy check always
-        this.lazyCheck = this.lazyCheck.bind(this);
-
-        // start listening for viewport events
-        window.addEventListener('scroll', this.lazyCheck);
-        window.addEventListener('resize', this.lazyCheck);
-
+        const iObserver = new IntersectionObserver((elemRect) => {
+            console.log(elemRect);
+            this.setState({ visible: elemRect[0].intersectionRatio > 0 });
+        }, { threshold: [0], rootMargin: '200% 0%' });
+        iObserver.observe(elem);
     }
 
-    lazyCheck() {
-        var element = this.refs.LazyElement;
-        var bounds = element.getBoundingClientRect();
-        var scrollTop = window.pageYOffset;
-        var top = bounds.top + scrollTop;
-        var height = bounds.bottom - bounds.top;
-        var distance = this.props.distance;
+    // componentDidMount() {
+    //
+    //     // initial check
+    //     this.lazyCheck();
+    //
+    //     // bind this to lazy check always
+    //     this.lazyCheck = this.lazyCheck.bind(this);
+    //
+    //     // start listening for viewport events
+    //     window.addEventListener('scroll', this.lazyCheck);
+    //     window.addEventListener('resize', this.lazyCheck);
+    //
+    // }
+    //
+    // lazyCheck() {
+    //     var element = this.refs.LazyElement;
+    //     var bounds = element.getBoundingClientRect();
+    //     var scrollTop = window.pageYOffset;
+    //     var top = bounds.top + scrollTop;
+    //     var height = bounds.bottom - bounds.top;
+    //     var distance = this.props.distance;
+    //
+    //     if (top === 0 || (top <= (scrollTop + window.innerHeight + distance) && (top + height + distance) > scrollTop)) {
+    //         this.setState({ visible: true });
+    //         this.removeListener(); // stop listening, the show is over
+    //     }
+    // }
 
-        if (top === 0 || (top <= (scrollTop + window.innerHeight + distance) && (top + height + distance) > scrollTop)) {
-            this.setState({ visible: true });
-            this.removeListener(); // stop listening, the show is over
-        }
-    }
-
-    removeListener() {
-        window.removeEventListener('scroll', this.lazyCheck);
-        window.removeEventListener('resize', this.lazyCheck);
-    }
+    // removeListener() {
+    //     window.removeEventListener('scroll', this.lazyCheck);
+    //     window.removeEventListener('resize', this.lazyCheck);
+    // }
 
     render() {
-        var style = Pod_Styler.getStyle(this);
+        const style = Pod_Styler.getStyle(this);
 
-        var placeholder = (this.props.placeholder) ? this.props.placeholder : '';
-        var children = (this.state.visible) ? this.props.children : placeholder;
+        const placeholder = (this.props.placeholder) ? this.props.placeholder : '';
+        const children = (this.state.visible) ? this.props.children : placeholder;
+
+        const content = (this.state.visible) ? children : placeholder;
 
         return (
             <div style={style.main} ref="LazyElement">
-                {children}
+                {content}
             </div>
         );
-
     }
 
 };
