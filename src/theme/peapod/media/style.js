e5d1d40e2a36bcca68ea2117f53ea83016609776
Damion Yeatman <damion@yeatman.me>
2016-07-26 17:15:38 +0100

    Media max width fix

--- src/theme/peapod/media/style.js
+++ src/theme/peapod/media/style.js
@@ -31,6 +31,7 @@ module.exports = function (sheet) {
             common: {
                 alignSelf: 'flex-start',
                 minWidth: 'getProp:figureWidth',
+                maxWidth: 'getProp:figureWidth',
             },
         }).addSelector({
             condition: ['figureMiddle'],
