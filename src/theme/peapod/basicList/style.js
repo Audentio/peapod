3b4f36eba342aa2a4d28dc9acb793fb91dc4d848
Tushar Singh <tusharsingh@gmx.com>
2016-09-22 02:24:55 +0530

    file case

--- src/theme/peapod/basicList/style.js
+++
@@ -1,7 +0,0 @@
-import { Sheet } from 'utility/stylesheet.js';
-
-module.exports = function (sheetName) {
-    const sheet = new Sheet(sheetName);
-
-    return sheet;
-};
--- src/theme/peapod/select/option/style.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (sheet) {
-    sheet.resolveStyles = (component, theme) => { // eslint-disable-line no-unused-vars
-        sheet.selector('.main', {
-            fontWeight: 'bold',
-        });
-    };
-
-    return sheet;
-};
