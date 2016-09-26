535ef4616f05f37e647b27acef5a2d8a9ec5629e
Kyler Carlson <kylerc@audentio.com>
2016-09-26 14:32:18 -0500

    changes to media coponent to be able to add a border to the figure

--- src/theme/peapod/media/style.js
+++ src/theme/peapod/media/style.js
@@ -8,6 +8,8 @@ module.exports = function (sheet) {
     sheet.addCondition('contentMiddle').addProp({ contentVertical: 'middle' });
     sheet.addCondition('contentBottom').addProp({ contentVertical: 'bottom' });
 
+    sheet.addCondition('borderFigure', obj => obj.props.borderFigure);
+
     sheet.resolveValues = theme => { // eslint-disable-line no-unused-vars
         const component = {};
         return component;
@@ -34,6 +36,13 @@ module.exports = function (sheet) {
                 alignSelf: 'flex-end',
             });
         }
+
+        sheet.selector('.figure.--borderFigure', {
+            padding: '10px',
+            backgroundColor: '#FFF',
+            margin: '6px',
+            boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+        });
     };
 
     return sheet;
